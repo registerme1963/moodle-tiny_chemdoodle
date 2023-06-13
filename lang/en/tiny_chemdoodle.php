1962e08140de63bdc4aa3037bc09d06369c78902
registerme1963 <45921124+registerme1963@users.noreply.github.com>
2023-06-13 11:48:02 +0530

    Create tiny_chemdoodle.php

---
+++ lang/en/tiny_chemdoodle.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'tiny_chemdoodle', language 'en'.
+ *
+ * @copyright  2023 Matt Davidson <davidso1@rose-hulman.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'ChemDoodle';
+$string['privacy:metadata'] = 'The ChemDoodle plugin does not save or export user data.';
+$string['sketchtitle'] = 'Chemdraw Structure and Save';
+$string['sketch_description'] = 'Quick draw chemical structure in Moodle';
+$string['sketch_long_description'] = 'Draw and edit chemical structures inside any editor in Moodle';
+$string['storeinrepo'] = 'Store as image file';
+$string['storeinrepo_desc'] = 'If checked the chemical structure will be saved as a standard image upload instead of a base64 inline image.';
+$string['insert'] = 'Insert Structure';
+$string['helplinktext'] = 'Insert Chemical Structure';
